49590142c1d3a9ed9ea2152986cacfb520edabde
eamesa <eamadorm@chicagobooth.edu>
2022-02-20 19:36:39 +0000

    photo index done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
+  get("/", {:controller => "users", :action => "index" })
   get("/users", {:controller => "users", :action => "index" })
   get("/users/:path_username", {:controller => "users", :action => "path_username" })
+
+  get("/photos", {:controller => "photos", :action => "index" })
+  #get("/users/:path_username", {:controller => "users", :action => "path_username" })
 
 end
