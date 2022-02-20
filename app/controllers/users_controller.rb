3b8ffcf19c1e7ab4d1c27e5c8e69aa5d8ea9a728
eamesa <eamadorm@chicagobooth.edu>
2022-02-20 19:00:56 +0000

    users done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,18 @@ class UsersController < ApplicationController
     @list_of_users = matching_users.order({ :username => :asc})
     render({ :template => "user_templates/index.html.erb"})
   end
+  
+  def path_username
+    
+    url_username = params.fetch("path_username")
+    
+    matching_usernames = User.where({ :username => url_username})
+    
+    @the_user = matching_usernames.first
+    if @the_user == nil
+      redirect_to ("/404")
+    else
+      render({ :template => "user_templates/path_username.html.erb"})
+  end
+end
 end
